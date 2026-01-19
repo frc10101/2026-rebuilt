c59f6197c9f0814ca7a930231b29a5301b714db4
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2026-01-19 10:34:15 -0600

    use parameter tests (#841)

--- src/test/java/frc/robot/util/HelpersTests.java
+++ src/test/java/frc/robot/util/HelpersTests.java
@@ -1,43 +1,33 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
+package frc.robot.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import frc.robot.util.Helpers;
 import java.util.Optional;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.MockedStatic;
 
 public class HelpersTests {
-  private MockedStatic<DriverStation> driveStationMock;
-
-  @BeforeEach
-  public void setUp() {
-    driveStationMock = mockStatic(DriverStation.class);
-  }
-
-  @AfterEach
-  public void tearDown() {
-    driveStationMock.close();
-  }
-
-  @Test
-  public void isAllianceHubActive_Test_OnRed_RedWins() {
-    driveStationMock.when(DriverStation::getGameSpecificMessage).thenReturn("R");
-    driveStationMock.when(DriverStation::getAlliance).thenReturn(Optional.of(Alliance.Red));
+  @DisplayName("Active Hub Test")
+  @ParameterizedTest(name = "On {1} Alliance with Game Data {0}")
+  @CsvSource({
+    "R,Red,true",
+    "B,Blue,true",
+    "R,Blue,false",
+    "B,Red,false",
+  })
+  public void isAllianceHubActive_Test_OnRed_RedWins(
+      String gameData, Alliance alliance, boolean isActiveExpected) {
+    MockedStatic<DriverStation> driverStationMock = mockStatic(DriverStation.class);
+    driverStationMock.when(DriverStation::getGameSpecificMessage).thenReturn(gameData);
+    driverStationMock.when(DriverStation::getAlliance).thenReturn(Optional.of(alliance));
 
     boolean isAllianceHubActive = Helpers.isAllianceHubActive();
-    assertTrue(isAllianceHubActive);
-  }
-
-  @Test
-  public void isAllianceHubActive_Test_OnBlue_BlueWins() {
-    driveStationMock.when(DriverStation::getGameSpecificMessage).thenReturn("B");
-    driveStationMock.when(DriverStation::getAlliance).thenReturn(Optional.of(Alliance.Blue));
-
-    boolean isAllianceHubActive = Helpers.isAllianceHubActive();
-    assertTrue(isAllianceHubActive);
+    assertEquals(isActiveExpected, isAllianceHubActive);
+    driverStationMock.close();
   }
 }
