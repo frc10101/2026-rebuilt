3133be07e2366a58f904b61e0520ab6caa3f7c98
CohenHill <cohen.hill@ascte.org>
2026-01-24 16:27:38 -0600

    added subsystems files

---
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//Belt Dexter maybe called Clyde reffering to all of hopper
+public class Indexer extends SubsystemBase {
+  /** Creates a new Indexer. */
+  public Indexer() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
