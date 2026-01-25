f1379bade5733b14eca890af42d526048ca0d0f1
NotMaxwell <William.mccormick@ascte.org>
2026-01-24 19:08:06 -0600

    Created motor config and motor config constatns for pacman pivot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,15 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -28,4 +37,44 @@ public final class Constants {
     /** Replaying from a log file. */
     REPLAY
   }
+
+  public final class Intake {
+    public final class Real {
+      public static final double kp = 50.0;
+      public static final double ki = 0.0;
+      public static final double kd = 0.0;
+      public static final AngularVelocity maxVelocity = DegreesPerSecond.of(90);
+      public static final AngularAcceleration maxAcceleration = DegreesPerSecondPerSecond.of(45);
+
+      public static final double ks = 0.0;
+      public static final double kg = 0.0;
+      public static final double kv = 0.0;
+    }
+
+    public final class Sim {
+      public static final double kp = 50.0;
+      public static final double ki = 0.0;
+      public static final double kd = 0.0;
+      public static final AngularVelocity maxVelocity = DegreesPerSecond.of(90);
+      public static final AngularAcceleration maxAcceleration = DegreesPerSecondPerSecond.of(45);
+
+      public static final double ks = 0.0;
+      public static final double kg = 0.0;
+      public static final double kv = 0.0;
+    }   
+
+    // Gears
+    // Write down gear stages from motor to output
+    // Example: 3:1 stage followed by 4:1 stage -> gearStages = {3, 4}
+    // If you gear up instead, use fractions like 1/3.0
+    public static final int[] gearStages = {3, 4};
+    public static final int totalGear = java.util.Arrays.stream(gearStages).reduce(1, (a, b) -> a * b);
+
+    // Motor properties from tutorial to prevent over currenting
+    public static final Current currentLimit = Amps.of(40);
+    public static final Time closedLoopRampRate = Seconds.of(0.);
+    public static final Time openLoopRampRate = Seconds.of(0.25);
+  }
+
+  public final class IDs {}
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,10 +4,51 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// Pacman
+import frc.robot.Constants;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 
+// Pacman
 public class Intake extends SubsystemBase {
+
+  private SmartMotorControllerConfig smcConfig =
+      new SmartMotorControllerConfig(this)
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          .withClosedLoopController(
+              Constants.Intake.Real.kp,
+              Constants.Intake.Real.ki,
+              Constants.Intake.Real.kd,
+              Constants.Intake.Real.maxVelocity,
+              Constants.Intake.Real.maxAcceleration)
+          .withSimClosedLoopController(
+              Constants.Intake.Sim.kp,
+              Constants.Intake.Sim.ki,
+              Constants.Intake.Sim.kd,
+              Constants.Intake.Sim.maxVelocity,
+              Constants.Intake.Sim.maxAcceleration)
+          .withFeedforward(
+              new ArmFeedforward(
+                  Constants.Intake.Real.ks, Constants.Intake.Real.kg, Constants.Intake.Real.kv))
+          .withSimFeedforward(
+              new ArmFeedforward(
+                  Constants.Intake.Sim.ks, Constants.Intake.Sim.kg, Constants.Intake.Sim.kv))
+          .withTelemetry("Intake", TelemetryVerbosity.HIGH)
+          // Gearing from motor rotor to final shaft
+          // Uses the pre-calculated totalGear from Constants (product of all gear stages)
+          // If you need to gear up instead, change totalGear calculation in Constants
+          .withGearing(Constants.Intake.totalGear)
+          //Motor properties from tutorial to prevent over currenting
+          .withMotorInverted(false)
+          .withIdleMode(MotorMode.BRAKE)
+          .withStatorCurrentLimit(Constants.Intake.currentLimit)
+          .withClosedLoopRampRate(Constants.Intake.closedLoopRampRate)
+          .withOpenLoopRampRate(Constants.Intake.openLoopRampRate);
+
+
   /** Creates a new Intake. */
   public Intake() {}
 
