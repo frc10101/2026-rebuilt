e5ca1caadaa0ae349cf5857a3ddcb4cb3f43d212
NotMaxwell <William.mccormick@ascte.org>
2026-01-25 15:01:25 -0600

    redid test to fix build error

--- src/test/java/frc/robot/subsystems/IntakeTests.java
+++ src/test/java/frc/robot/subsystems/IntakeTests.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj2.command.Command;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,98 +18,41 @@ public class IntakeTests {
     mIntake = new Intake();
   }
 
-  @AfterEach
-  void tearDown() {
-    mIntake.close();
-  }
-
   @Test
   void canCreateIntakeTest() {
     assertNotNull(mIntake);
   }
 
-  // Pivot Tests
-
   @Test
   void getPivotAngleNotNullTest() {
-    // getPivotAngle should return an Angle (even if zero)
     assertNotNull(mIntake.getPivotAngle());
   }
 
   @Test
+  void getRollerVelocityNotNullTest() {
+    assertNotNull(mIntake.getRollerVelocity());
+  }
+
+  @Test
   void setAngleReturnsCommandTest() {
-    // setAngle should return a Command that can be scheduled
     Command cmd = mIntake.setAngle(Degrees.of(0));
     assertNotNull(cmd);
   }
 
   @Test
   void setDutyCycleReturnsCommandTest() {
-    // set(dutyCycle) should return a Command that can be scheduled
     Command cmd = mIntake.set(0.5);
     assertNotNull(cmd);
   }
 
   @Test
-  void runSysIdReturnsCommandTest() {
-    // runSysId should return a Command for system identification
-    Command cmd = mIntake.runSysId();
-    assertNotNull(cmd);
-  }
-
-  // Roller Tests
-
-  @Test
-  void getRollerVelocityNotNullTest() {
-    // getRollerVelocity should return an AngularVelocity (even if zero)
-    assertNotNull(mIntake.getRollerVelocity());
-  }
-
-  @Test
   void setRollerReturnsCommandTest() {
-    // setRoller should return a Command that can be scheduled
     Command cmd = mIntake.setRoller(0.5);
     assertNotNull(cmd);
   }
 
   @Test
-  void intakeReturnsCommandTest() {
-    // intake() should return a Command for intaking game pieces
-    Command cmd = mIntake.intake();
-    assertNotNull(cmd);
-  }
-
-  @Test
-  void outtakeReturnsCommandTest() {
-    // outtake() should return a Command for outtaking game pieces
-    Command cmd = mIntake.outtake();
-    assertNotNull(cmd);
-  }
-
-  @Test
-  void stopRollerReturnsCommandTest() {
-    // stopRoller() should return a Command to stop the roller
-    Command cmd = mIntake.stopRoller();
-    assertNotNull(cmd);
-  }
-
-  // Lifecycle Tests
-
-  @Test
-  void periodicDoesNotThrowTest() {
-    // Calling periodic should not throw exceptions
-    assertDoesNotThrow(() -> mIntake.periodic());
-  }
-
-  @Test
-  void simulationPeriodicDoesNotThrowTest() {
-    // Calling simulationPeriodic should not throw exceptions
-    assertDoesNotThrow(() -> mIntake.simulationPeriodic());
-  }
-
-  @Test
-  void periodicAndSimulationPeriodicDontThrowTest() {
-    // Calling both lifecycle methods together should not throw exceptions
+  void periodicAndSimulationDontThrow() {
     assertDoesNotThrow(
         () -> {
           mIntake.periodic();
